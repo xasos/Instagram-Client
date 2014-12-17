14d7750a41a6c7884a015a4c60169b6b0b832622
Niraj Pant <nirajpant7@gmail.com>
2014-12-16 23:20:16 -0600

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,21 +1,34 @@
-angular.module('Instagram', ['ngRoute', 'ngMessages'])
-	.config(function($routeProvider) {
-		$routeProvider
-		  .when('/', {
-		    templateUrl: 'views/home.html',
-		    controller: 'HomeCtrl'
-		  })
-		  .when('/login', {
-		    templateUrl: 'views/login.html',
-		    controller: 'LoginCtrl'
-		  })
-		  .when('/signup', {
-		    templateUrl: 'views/signup.html',
-		    controller: 'SignupCtrl'
-		  })
-		  .when('/photo/:id', {
-		    templateUrl: 'views/detail.html',
-		    controller: 'DetailCtrl'
-		  })
-		  .otherwise('/');			
-	});
+angular.module('Instagram', ['ngRoute', 'ngMessages', 'satellizer'])
+    .config(function($routeProvider, $authProvider) {
+        $routeProvider
+          .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'HomeCtrl'
+          })
+          .when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'LoginCtrl'
+          })
+          .when('/signup', {
+            templateUrl: 'views/signup.html',
+            controller: 'SignupCtrl'
+          })
+          .when('/photo/:id', {
+            templateUrl: 'views/detail.html',
+            controller: 'DetailCtrl'
+          })
+          .otherwise('/');
+          
+          $authProvider.loginUrl = 'http://localhost:3000/auth/login';
+          $authProvider.signupUrl = 'http://localhost:3000/auth/signup';
+          $authProvider.oauth2({
+              name: 'instagram',
+              url: 'http://localhost:3000/auth/instagram',
+              redirectUri: 'http://localhost:8000',
+              clientId: '799d1f8ea0e44ac8b70e7f18fcacedd1',
+              requiredUrlParams: ['scope'],
+              scope: ['likes'],
+              scopeDelimiter: '+',
+              authorizationEndpoint: 'https://api.instagram.com/oauth/authorize'
+});
+    });
