f1cf3df870df632f91a0d6aac4f2987407b8e005
Niraj Pant <nirajpant7@gmail.com>
2014-12-16 22:30:13 -0600

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,21 +1,21 @@
 angular.module('Instagram', ['ngRoute', 'ngMessages'])
 	.config(function($routeProvider) {
-$routeProvider
-  .when('/', {
-    templateUrl: 'views/home.html',
-    controller: 'HomeCtrl'
-  })
-  .when('/login', {
-    templateUrl: 'views/login.html',
-    controller: 'LoginCtrl'
-  })
-  .when('/signup', {
-    templateUrl: 'views/signup.html',
-    controller: 'SignupCtrl'
-  })
-  .when('/photo/:id', {
-    templateUrl: 'views/detail.html',
-    controller: 'DetailCtrl'
-  })
-  .otherwise('/');			
+		$routeProvider
+		  .when('/', {
+		    templateUrl: 'views/home.html',
+		    controller: 'HomeCtrl'
+		  })
+		  .when('/login', {
+		    templateUrl: 'views/login.html',
+		    controller: 'LoginCtrl'
+		  })
+		  .when('/signup', {
+		    templateUrl: 'views/signup.html',
+		    controller: 'SignupCtrl'
+		  })
+		  .when('/photo/:id', {
+		    templateUrl: 'views/detail.html',
+		    controller: 'DetailCtrl'
+		  })
+		  .otherwise('/');			
 	});
