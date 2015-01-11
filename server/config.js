288553b938bf930ff8947cf3efa6b6f31223220c
Niraj Pant <nirajpant7@gmail.com>
2015-01-11 00:31:33 -0600

    fix key issue

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     db: 'localhost',
-    clientSecret: process.env.clientSecret || ' 1e2db1edbf0b4d6d886ea2a046edcec',
+    clientSecret: process.env.clientSecret || '1e2db1edbf0b4d6d886ea2a046edcec',
     tokenSecret: process.env.tokenSecret || 'olko29aj29y4na-30cn2mxja'
 }
